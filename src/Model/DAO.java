a9fde2c329d341edd1c3b3f78a242454debc856f
rileyjsumner <rsumner@wisc.edu>
2018-10-07 17:39:33 -0500

    Created support for int and double sql methods

--- src/Model/DAO.java
+++ src/Model/DAO.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * @class DAO
  *
  * Will be used to manipulate and get information from the DB
  */
@@ -47,7 +46,7 @@ public class DAO {
    * @param tablename is the name of table which data is inserting into
    * @param insertVals Map where first index is the column, second index is the value
    */
-  public static void insert(String tablename, Map<String, String> insertVals) {
+  public static void insertStr(String tablename, Map<String, String> insertVals) {
     String columnList = "";
     String valueList = "";
     PreparedStatement preparedStatement;
@@ -70,13 +69,62 @@ public class DAO {
   }
 
   /**
+   *
+   * @param tablename
+   * @param insertVals
+   */
+  public static void insertInt(String tablename, Map<String, Integer> insertVals) {
+    String columnList = "";
+    String valueList = "";
+    PreparedStatement preparedStatement;
+    for(Map.Entry<String, Integer> entry : insertVals.entrySet()) {
+      columnList += entry.getKey()+", ";
+      valueList  += "?,";
+    }
+
+    try {
+      // INSERT into tablename (col1, col2) VALUES (?, ?);
+      preparedStatement = connection.prepareStatement("INSERT INTO " + tablename + "(" + columnList + ") VALUES (" + valueList + ");");
+      int i = 1;
+      for(Map.Entry<String, Integer> entry : insertVals.entrySet()) {
+        preparedStatement.setInt(i, entry.getValue());
+        i++;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void insertDouble(String tablename, Map<String, Double> insertVals) {
+    String columnList = "";
+    String valueList = "";
+    PreparedStatement preparedStatement;
+    for(Map.Entry<String, Double> entry : insertVals.entrySet()) {
+      columnList += entry.getKey()+", ";
+      valueList  += "?,";
+    }
+
+    try {
+      // INSERT into tablename (col1, col2) VALUES (?, ?);
+      preparedStatement = connection.prepareStatement("INSERT INTO " + tablename + "(" + columnList + ") VALUES (" + valueList + ");");
+      int i = 1;
+      for(Map.Entry<String, Double> entry : insertVals.entrySet()) {
+        preparedStatement.setDouble(i, entry.getValue());
+        i++;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
    * Update method for the database
    * @param tablename table to update
    * @param updateVals
    * @param colCheck
    * @param valCheck
    */
-  public static void update(String tablename, Map<String, String> updateVals, String colCheck, String valCheck) {
+  public static void updateStr(String tablename, Map<String, String> updateVals, String colCheck, String valCheck) {
     String updateList = "";
     PreparedStatement preparedStatement;
     for(Map.Entry<String, String> entry : updateVals.entrySet()){
@@ -96,10 +144,50 @@ public class DAO {
     }
   }
 
+  public static void updateInt(String tablename, Map<String, Integer> updateVals, String colCheck, String valCheck) {
+    String updateList = "";
+    PreparedStatement preparedStatement;
+    for(Map.Entry<String, Integer> entry : updateVals.entrySet()){
+      updateList+=entry.getKey()+" = ?,";
+    }
+    try {
+      // UPDATE tablename SET col1 = ?, col2 = ? WHERE col3 = ?
+      preparedStatement = connection.prepareStatement("UPDATE " + tablename + " SET " + updateList + " WHERE " + colCheck +" = ?;");
+      int i = 1;
+      for(Map.Entry<String, Integer> entry : updateVals.entrySet()){
+        preparedStatement.setInt(i, entry.getValue());
+        i++;
+      }
+      preparedStatement.setString(i, valCheck);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void updateDouble(String tablename, Map<String, Double> updateVals, String colCheck, String valCheck) {
+    String updateList = "";
+    PreparedStatement preparedStatement;
+    for(Map.Entry<String, Double> entry : updateVals.entrySet()){
+      updateList+=entry.getKey()+" = ?,";
+    }
+    try {
+      // UPDATE tablename SET col1 = ?, col2 = ? WHERE col3 = ?
+      preparedStatement = connection.prepareStatement("UPDATE " + tablename + " SET " + updateList + " WHERE " + colCheck +" = ?;");
+      int i = 1;
+      for(Map.Entry<String, Double> entry : updateVals.entrySet()){
+        preparedStatement.setDouble(i, entry.getValue());
+        i++;
+      }
+      preparedStatement.setString(i, valCheck);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
   /**
    * Get information from the database
    */
-  public static ArrayList<String> select(String tablename, ArrayList<String> colSelect, String whereCol, String whereVal) {
+  public static ArrayList<String> selectStr(String tablename, ArrayList<String> colSelect, String whereCol, String whereVal) {
     ArrayList<String> collection = new ArrayList<>();
     PreparedStatement preparedStatement;
     String columnList = "";
@@ -117,5 +205,39 @@ public class DAO {
     return collection;
   }
 
+  public static ArrayList<Integer> selectInt(String tablename, ArrayList<Integer> colSelect, String whereCol, String whereVal) {
+    ArrayList<Integer> collection = new ArrayList<>();
+    PreparedStatement preparedStatement;
+    String columnList = "";
+
+    for(int col : colSelect) {
+      columnList+=col + ((colSelect.indexOf(col) != colSelect.size()) ? ",":"");
+    }
+    try {
+      // SELECT col1, col2 FROM tablename WHERE col3 = val;
+      preparedStatement = connection.prepareStatement("SELECT " + columnList + " FROM " + tablename + " WHERE " + whereCol + " = ?;");
+      preparedStatement.setString(1, whereVal);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return collection;
+  }
+  public static ArrayList<String> selectDouble(String tablename, ArrayList<Double> colSelect, String whereCol, String whereVal) {
+    ArrayList<String> collection = new ArrayList<>();
+    PreparedStatement preparedStatement;
+    String columnList = "";
+
+    for(Double col : colSelect) {
+      columnList+=col.toString() + ((colSelect.indexOf(col) != colSelect.size()) ? ",":"");
+    }
+    try {
+      // SELECT col1, col2 FROM tablename WHERE col3 = val;
+      preparedStatement = connection.prepareStatement("SELECT " + columnList + " FROM " + tablename + " WHERE " + whereCol + " = ?;");
+      preparedStatement.setString(1, whereVal);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return collection;
+  }
 
 }
