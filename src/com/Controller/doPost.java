4b98364ad2790d00ec399818465f02530027d223
jpierro0513 <jmpierro0513@gmail.com>
2018-10-25 23:36:54 -0500

    added post and put interfaces

---
+++ src/com/Controller/doPost.java
@@ -0,0 +1,8 @@
+package com.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface doPost {
+    void doPost(HttpServletRequest request, HttpServletResponse response);
+}
