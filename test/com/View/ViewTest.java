2e8b0ea617a0ae01239d1b855359ed396015d034
rileyjsumner <rsumner@wisc.edu>
2018-12-01 15:23:59 -0600

    first test for View

--- test/com/View/ViewTest.java
+++ test/com/View/ViewTest.java
@@ -1,5 +1,6 @@
 package com.View;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import javax.swing.*;
@@ -16,6 +17,7 @@ public class ViewTest {
   public void testCreateWindow() {
     View view = new View();
     JFrame frame = view.createWindow("Frame Title", 150, 200);
+    Assert.assertEquals(true, frame.getHeight() == 200 && frame.getWidth() == 150 && frame.getTitle().equals("Frame Title"));
   }
 
 }
