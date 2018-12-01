a6aef1b7017a559d655c4fed0aff439794bce299
jpierro0513 <jmpierro0513@gmail.com>
2018-12-01 16:35:49 -0600

    added hella GUI methods per #22

--- src/com/View/View.java
+++ src/com/View/View.java
@@ -1,13 +1,151 @@
 package com.View;
 
 import javax.swing.*;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.io.File;
 
 public class View {
-    public  JFrame createWindow(String title, int width, int height){
+    public JFrame createWindow(String title, int width, int height){
         JFrame frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(width, height);
         frame.setVisible(true);
         return frame;
     }
+
+    public JPanel addPanel(JFrame frame){
+        JPanel panel = new JPanel();
+        frame.getContentPane().add(panel);
+        return panel;
+    }
+
+    public JButton addNewButton(String title, JFrame frame){
+        JButton button = new JButton(title);
+        frame.getContentPane().add(button);
+        return button;
+    }
+
+    public JButton addNewButton(Icon icon, JFrame frame){
+        JButton button = new JButton(icon);
+        frame.getContentPane().add(button);
+        return button;
+    }
+
+    public JButton addNewButton(String title, Icon icon, JFrame frame){
+        JButton button = new JButton(title, icon);
+        frame.getContentPane().add(button);
+        return button;
+    }
+
+    public JCheckBox addCheckBox (String title, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(title);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBox addCheckBox (Icon icon, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(icon);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBox addCheckBox (String title, Icon icon, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(title, icon);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBox addCheckBox (String title, boolean selected, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(title, selected);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBox addCheckBox (Icon icon, boolean selected, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(icon, selected);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBox addCheckBox (String title, Icon icon, boolean selected, JFrame frame){
+        JCheckBox checkBox = new JCheckBox(title, icon, selected);
+        frame.getContentPane().add(checkBox);
+        return checkBox;
+    }
+
+    public JCheckBoxMenuItem addCheckBoxMenuItem (String title, JFrame frame){
+        JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem(title);
+        frame.getContentPane().add(checkBoxMenuItem);
+        return checkBoxMenuItem;
+    }
+
+    public JCheckBoxMenuItem addCheckBoxMenuItem (Icon icon, JFrame frame){
+        JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem(icon);
+        frame.getContentPane().add(checkBoxMenuItem);
+        return checkBoxMenuItem;
+    }
+
+    public JCheckBoxMenuItem addCheckBoxMenuItem (String title, Icon icon, JFrame frame){
+        JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem(title, icon);
+        frame.getContentPane().add(checkBoxMenuItem);
+        return checkBoxMenuItem;
+    }
+
+    public JCheckBoxMenuItem addCheckBoxMenuItem (String title, boolean selected, JFrame frame){
+        JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem(title, selected);
+        frame.getContentPane().add(checkBoxMenuItem);
+        return checkBoxMenuItem;
+    }
+
+    public JCheckBoxMenuItem addCheckBoxMenuItem (String title, Icon icon, boolean selected, JFrame frame){
+        JCheckBoxMenuItem checkBoxMenuItem = new JCheckBoxMenuItem(title, icon, selected);
+        frame.getContentPane().add(checkBoxMenuItem);
+        return checkBoxMenuItem;
+    }
+
+    public JColorChooser addColorChooser (Color color, JFrame frame){
+        JColorChooser colorChooser = new JColorChooser(color);
+        frame.getContentPane().add(colorChooser);
+        return colorChooser;
+    }
+
+    public JColorChooser addColorChooser (ColorSelectionModel model, JFrame frame){
+        JColorChooser colorChooser = new JColorChooser(model);
+        frame.getContentPane().add(colorChooser);
+        return colorChooser;
+    }
+
+    public JFileChooser addFileChooser (File currentDirectory, JFrame frame){
+        JFileChooser fileChooser = new JFileChooser(currentDirectory);
+        frame.getContentPane().add(fileChooser);
+        return fileChooser;
+    }
+
+    public JFileChooser addFileChooser (FileSystemView fav, JFrame frame){
+        JFileChooser fileChooser = new JFileChooser(fav);
+        frame.getContentPane().add(fileChooser);
+        return fileChooser;
+    }
+
+    public JFileChooser addFileChooser (String currentDirectoryPath, JFrame frame){
+        JFileChooser fileChooser = new JFileChooser(currentDirectoryPath);
+        frame.getContentPane().add(fileChooser);
+        return fileChooser;
+    }
+
+    public JFileChooser addFileChooser (File currentDirectory, FileSystemView fav, JFrame frame){
+        JFileChooser fileChooser = new JFileChooser(currentDirectory, fav);
+        frame.getContentPane().add(fileChooser);
+        return fileChooser;
+    }
+
+    public JFileChooser addFileChooser (String currentDirectoryPath, FileSystemView fav, JFrame frame){
+        JFileChooser fileChooser = new JFileChooser(currentDirectoryPath, fav);
+        frame.getContentPane().add(fileChooser);
+        return fileChooser;
+    }
+    
+
 }
