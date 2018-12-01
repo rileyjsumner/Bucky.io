1a7fb33944ca7856be30557b45983ee58fe6adc7
jpierro0513 <jmpierro0513@gmail.com>
2018-12-01 15:15:53 -0600

    Wrote createWindow method

--- src/com/View/View.java
+++ src/com/View/View.java
@@ -1,4 +1,13 @@
 package com.View;
 
+import javax.swing.*;
+
 public class View {
+    public static JFrame createWindow(String title, int width, int height){
+        JFrame frame = new JFrame(title);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(width, height);
+        frame.setVisible(true);
+        return frame;
+    }
 }
